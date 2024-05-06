23e6b7b8195b260ecc3c52eea50fdd01d12f7691
Ankur <ankur.vekariya.1707@gmail.com>
2024-05-06 12:46:54 +0530

    minor changes

--- LazyImageLoader/Classes/Manager/ImageLoader.swift
+++ LazyImageLoader/Classes/Manager/ImageLoader.swift
@@ -14,27 +14,30 @@ class ImageLoader {
     private var uuidMap = [Int: UUID]()
     
     private init() {}
+    let dispatchGroup = DispatchQueue(label: "com.mobile.imageloader")
     
     func load(_ url: NSURL, index: Int, completion: @escaping (UIImage?, Int) -> Void) {
         
-        let token = ImagesCatch.publicCache.load(url: url, completion: { image in
-            defer {
-                self.uuidMap.removeValue(forKey: index)
+        let workItem = DispatchWorkItem {
+            let token = ImagesCatch.publicCache.load(url: url, completion: { image in
+                defer {
+                    self.uuidMap.removeValue(forKey: index)
+                }
+                DispatchQueue.main.async {
+                    completion(image, index)
+                }
+            })
+            if let token = token {
+                self.uuidMap[index] = token
             }
-            DispatchQueue.main.async {
-                completion(image, index)
-            }
-        })
-
-         if let token = token {
-           uuidMap[index] = token
-         }
+        }
+        dispatchGroup.sync(execute: workItem)
+        
     }
     
     func cancel(for index: Int) {
         if let uuid = uuidMap[index] {
             imageLoader.cancelLoad(uuid)
-            
             uuidMap.removeValue(forKey: index)
         }
     }
